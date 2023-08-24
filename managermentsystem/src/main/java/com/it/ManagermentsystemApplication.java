6561e72ba6258534422f98f74c7fe87202b6f863
WZ <525321538@qq.com>
2023-08-24 21:31:12 +0800

    后端提交

---
+++ managermentsystem/src/main/java/com/it/ManagermentsystemApplication.java
@@ -0,0 +1,13 @@
+package com.it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagermentsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagermentsystemApplication.class, args);
+	}
+
+}
